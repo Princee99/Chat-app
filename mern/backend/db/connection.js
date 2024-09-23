675a20117ccba06f04fecfc9565dee7dd724ffdd
22IT070 Henil <22it070@charusat.edu.in>
2024-09-23 15:30:41 +0530

    first commit

--- mern/backend/db/connection.js
+++ mern/backend/db/connection.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const URI = "mongodb://mongodb:27017";
+const URI = "mongodb+srv://22it106:cLustermongo55@cluster0.2gewc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
